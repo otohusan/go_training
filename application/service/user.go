17d4bc7311795967b31541c9593473c52af915f4
otohusan <tamioguitar@gmail.com>
2024-03-05 18:09:25 +0900

    feat: userServiceの作成

---
+++ application/service/user.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"context"
+	"go-training/domain/model"
+	"go-training/domain/repository"
+)
+
+type UserService struct {
+	repo repository.UserRepository
+}
+
+// NewUserService は新しいUserServiceオブジェクトを作成します。
+func NewUserService(repo repository.UserRepository) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+func (s *UserService) FindByID(ctx context.Context, id uint) (*model.User, error) {
+	return s.repo.FindByID(ctx, id)
+}
+
+func (s *UserService) Create(ctx context.Context, user *model.User) error {
+	return s.repo.Create(ctx, user)
+}
