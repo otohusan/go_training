2c2f6fdf0d925f871309d575f883c34943225b0c
otohusan <tamioguitar@gmail.com>
2024-03-06 13:16:34 +0900

    ドメイン駆動のアーキテクチャをテスト

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"go-training/application/service"
+	"go-training/domain/model"
+	userRepo "go-training/infrastructure/rest"
+)
+
+func main() {
+	user := model.User{
+		ID:   213141,
+		Name: "sasas",
+	}
+
+	userReposit := userRepo.NewUserRepositoryImpl()
+
+	userService := service.NewUserService(userReposit)
+
+	retunr, error := userService.FindByID(user, 2112)
+	if error != nil {
+		return
+	}
+
+	println(retunr)
+}
--- domain/model/user.go
+++ domain/model/user.go
@@ -1,7 +1,29 @@
 package model
 
+import "time"
+
 // User はユーザー情報を表すドメインモデルです。
 type User struct {
 	ID   uint   `json:"id"`
 	Name string `json:"name"`
+}
+
+// Deadline implements context.Context.
+func (User) Deadline() (deadline time.Time, ok bool) {
+	panic("unimplemented")
+}
+
+// Done implements context.Context.
+func (User) Done() <-chan struct{} {
+	panic("unimplemented")
+}
+
+// Err implements context.Context.
+func (User) Err() error {
+	panic("unimplemented")
+}
+
+// Value implements context.Context.
+func (User) Value(key any) any {
+	panic("unimplemented")
 }
