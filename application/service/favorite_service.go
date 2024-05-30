83b042750181e15e11aae5ed778dfc33ba23212d
otohusan <tamioguitar@gmail.com>
2024-05-30 11:29:52 +0900

    feat: favoriteのservice作成

---
+++ application/service/favorite_service.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"go-training/domain/model"
+	"go-training/domain/repository"
+)
+
+type FavoriteService struct {
+	favoriteRepo repository.FavoriteRepository
+}
+
+func NewFavoriteService(favoriteRepo repository.FavoriteRepository) *FavoriteService {
+	return &FavoriteService{
+		favoriteRepo: favoriteRepo,
+	}
+}
+
+func (s *FavoriteService) AddFavorite(userID, studySetID string) error {
+	return s.favoriteRepo.AddFavorite(userID, studySetID)
+}
+
+func (s *FavoriteService) RemoveFavorite(userID, studySetID string) error {
+	return s.favoriteRepo.RemoveFavorite(userID, studySetID)
+}
+
+func (s *FavoriteService) GetFavoritesByUserID(userID string) ([]*model.Favorite, error) {
+	return s.favoriteRepo.GetFavoritesByUserID(userID)
+}
+
+func (s *FavoriteService) IsFavorite(userID, studySetID string) (bool, error) {
+	return s.favoriteRepo.IsFavorite(userID, studySetID)
+}
