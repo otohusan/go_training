4068458fd326a3093ed5c2a220052d148a1d4db9
otohusan <tamioguitar@gmail.com>
2024-05-30 11:39:03 +0900

    feat: favoriteのroute作成

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	flashcard "go-training/infrastructure/InMemory/FlashCard"
 	studySet "go-training/infrastructure/InMemory/StudySet"
 	user "go-training/infrastructure/InMemory/User"
+	favorite "go-training/infrastructure/inmemory/Favorite"
 )
 
 func main() {
@@ -18,28 +19,32 @@ func main() {
 	userRepo := user.NewUserRepository()
 	studySetRepo := studySet.NewStudySetRepository()
 	flashcardRepo := flashcard.NewFlashcardRepository()
+	favoriteRepo := favorite.NewFavoriteRepository()
 
 	// サービスの初期化
 	userService := service.NewUserService(userRepo)
 	studySetService := service.NewStudySetService(studySetRepo)
 	flashcardService := service.NewFlashcardService(flashcardRepo)
+	favoriteService := service.NewFavoriteService(favoriteRepo)
 
 	// ハンドラーの初期化
 	userHandler := handlers.NewUserHandler(userService)
 	studySetHandler := handlers.NewStudySetHandler(studySetService)
 	flashcardHandler := handlers.NewFlashcardHandler(flashcardService)
+	favoriteHandler := handlers.NewFavoriteHandler(favoriteService)
 
 	// Ginのルーターを設定
 	router := gin.Default()
 
 	// ルートの設定
-	setupRoutes(router, userHandler, studySetHandler, flashcardHandler)
+	setupRoutes(router, userHandler, studySetHandler, flashcardHandler, favoriteHandler)
 
 	// サーバーの起動
 	router.Run(":8080")
 }
 
-func setupRoutes(router *gin.Engine, userHandler *handlers.UserHandler, studySetHandler *handlers.StudySetHandler, flashcardHandler *handlers.FlashcardHandler) {
+func setupRoutes(router *gin.Engine, userHandler *handlers.UserHandler, studySetHandler *handlers.StudySetHandler,
+	flashcardHandler *handlers.FlashcardHandler, favoriteHandler *handlers.FavoriteHandler) {
 	// ユーザー関連のルートをグループ化
 	userRoutes := router.Group("/users")
 	{
@@ -70,5 +75,13 @@ func setupRoutes(router *gin.Engine, userHandler *handlers.UserHandler, studySet
 		flashcardRoutes.GET("/studyset/:studySetID", flashcardHandler.GetFlashcardsByStudySetID)
 		flashcardRoutes.PUT("/:id", flashcardHandler.UpdateFlashcard)
 		flashcardRoutes.DELETE("/:id", flashcardHandler.DeleteFlashcard)
+	}
+
+	favoriteRoutes := router.Group("favorite")
+	{
+		favoriteRoutes.POST("/user/:userID/studyset/:studySetID", favoriteHandler.AddFavorite)
+		favoriteRoutes.DELETE("/user/:userID/studyset/:studySetID", favoriteHandler.RemoveFavorite)
+		favoriteRoutes.GET("/user/:userID", favoriteHandler.GetFavoritesByUserID)
+		favoriteRoutes.GET("/is_favorite", favoriteHandler.IsFavorite)
 	}
 }
