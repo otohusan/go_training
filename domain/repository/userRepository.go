bdf4245703a985377b836461deec3081a28ca616
otohusan <tamioguitar@gmail.com>
2024-06-01 09:17:33 +0900

    chore: 悩ましい部分にコメント追加

--- domain/repository/userRepository.go
+++ domain/repository/userRepository.go
@@ -8,6 +8,8 @@ type UserRepository interface {
 	GetByUsername(username string) (*model.UserResponse, error)
 	Update(user *model.User) error
 	Delete(id string) error
+	// 名前良くないかも
+	// これはログイン時に使うメソッドだからパスワードを含む構造体を使う
 	GetByEmail(email string) (*model.User, error)
 	GetAll() ([]*model.User, error)
 }
