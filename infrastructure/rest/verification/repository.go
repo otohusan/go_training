651e80a520291afa64be75b4866acd8745a2b183
otohusan <tamioguitar@gmail.com>
2024-06-03 17:10:14 +0900

    feat: DB用のGetVerificationInfoByTokenを作成

--- infrastructure/rest/verification/repository.go
+++ infrastructure/rest/verification/repository.go
@@ -2,6 +2,7 @@ package verification
 
 import (
 	"database/sql"
+	"errors"
 	"go-training/domain/model"
 	"go-training/domain/repository"
 )
@@ -24,7 +25,19 @@ func (r *VerificationRepository) SaveVerificationToken(verificationValue *model.
 }
 
 func (r *VerificationRepository) GetVerificationInfoByToken(token string) (*model.EmailVerification, error) {
-	return nil, nil
+	query := `SELECT email, token, username, password FROM email_verifications WHERE token = $1`
+	row := r.db.QueryRow(query, token)
+
+	verification := &model.EmailVerification{}
+	err := row.Scan(&verification.Email, &verification.Token, &verification.Username, &verification.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("invalid or expired token")
+		}
+		return nil, err
+	}
+
+	return verification, nil
 }
 
 func (r *VerificationRepository) DeleteVerificationToken(token string) error {
