86a20d9a9853ca742597835d402fb4bfc7aed5c1
otohusan <tamioguitar@gmail.com>
2024-06-03 15:43:49 +0900

    fix: smtp情報を環境変数から取得するようにと、送信メールの文面を編集

--- utils/email.go
+++ utils/email.go
@@ -3,22 +3,23 @@ package utils
 import (
 	"fmt"
 	"net/smtp"
+	"os"
 )
 
 // TODO: ホストの情報は環境変数から取得に変更
 func SendVerificationEmail(email, token string) error {
-	// SMTPサーバーの設定
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
-	from := "your-email@gmail.com"    // 送信者のメールアドレス
-	password := "your-email-password" // 送信者のメールアドレスのパスワード
+	// 環境変数からSMTP設定を読み込む
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort := os.Getenv("SMTP_PORT")
+	from := os.Getenv("SMTP_EMAIL")
+	password := os.Getenv("SMTP_PASSWORD")
 
 	// 受信者のメールアドレス
 	to := []string{email}
 
 	// メールの内容
-	subject := "Email Verification"
-	body := fmt.Sprintf("Please verify your email by clicking the link: http://localhost:8080/verify?token=%s", token)
+	subject := "本登録のご案内<自動送信>"
+	body := fmt.Sprintf("Konwalk(コンウォーク)に仮登録いただききありがとうございます。\n下記のリンクをクリックし、本登録を完了させてください。\n\nhttp://localhost:8080/verify?token=%s", token)
 	message := fmt.Sprintf("Subject: %s\n\n%s", subject, body)
 
 	// 認証情報
