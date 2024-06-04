5ac31cfe3eed9b7464b0aec73e21de9e8ed0a03e
otohusan <tamioguitar@gmail.com>
2024-06-04 22:26:26 +0900

    fix: flashcardをjson対応

--- domain/model/flashcard.go
+++ domain/model/flashcard.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Flashcard struct {
-	ID         string
-	StudySetID string
-	Question   string
-	Answer     string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+	ID         string    `json:"id"`
+	StudySetID string    `json:"study_set_id"`
+	Question   string    `json:"question"`
+	Answer     string    `json:"answer"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
