9de9129e413a7c0b521f19825694496e2f5aed3d
otohusan <tamioguitar@gmail.com>
2024-06-04 20:32:46 +0900

    fix: StudySetのモデルを改良

--- domain/model/studyset.go
+++ domain/model/studyset.go
@@ -3,10 +3,11 @@ package model
 import "time"
 
 type StudySet struct {
-	ID          string
-	UserID      string
-	Title       string
-	Description string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          string       `json:"id"`
+	UserID      string       `json:"user_id"`
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	Flashcards  []*Flashcard `json:"flashcards"`
 }
