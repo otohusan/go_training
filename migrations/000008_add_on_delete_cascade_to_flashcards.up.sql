486fec24c2d28af4b6381f25d0f4df961f21b8ba
otohusan <tamioguitar@gmail.com>
2024-06-13 16:55:42 +0900

    chore: フラッシュカードが存在しても学習セット消せるように、ON DELETE CASCADEをflashcardsテーブルの外部キーに設定

---
+++ migrations/000008_add_on_delete_cascade_to_flashcards.up.sql
@@ -0,0 +1,7 @@
+-- 既存の外部キー制約を削除
+ALTER TABLE flashcards DROP CONSTRAINT IF EXISTS flashcards_study_set_id_fkey;
+
+-- 新しい外部キー制約を追加
+ALTER TABLE flashcards
+ADD CONSTRAINT flashcards_study_set_id_fkey
+FOREIGN KEY (study_set_id) REFERENCES study_sets(id) ON DELETE CASCADE;
