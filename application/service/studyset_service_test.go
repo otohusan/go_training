f30e01f0a15e0a646e6f2a86e74a77f34f98e3e8
otohusan <tamioguitar@gmail.com>
2024-09-09 13:27:10 +0900

    test: FlashcardRepoのモックを作成

--- application/service/studyset_service_test.go
+++ application/service/studyset_service_test.go
@@ -1,7 +1,24 @@
 package service_test
 
-import "testing"
+import (
+	"testing"
+
+	gomock "go.uber.org/mock/gomock"
+
+	"go-training/application/service"
+	"go-training/domain/repository/mocks"
+)
 
 func TestGetStudySetByID(t *testing.T) {
+	// GoMockコントローラーの初期化
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// モックサービスの作成
+	mockStudySetRepo := mocks.NewMockStudySetRepository(ctrl)
+	mockFlashcardRepo := mocks.NewMockFlashcardRepository(ctrl)
+
+	// サービスを初期化
+	studySetService := service.NewStudySetService(mockStudySetRepo, mockFlashcardRepo)
 
 }
